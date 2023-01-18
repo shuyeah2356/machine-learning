248441c823ec29cc80ddd810e77f4708475c9727
Zhaocb <zhaocb@ebondhm.com>
2023-01-18 15:16:33 +0800

    线性判别分析算法更新

--- train_utils.py
+++ train_utils.py
@@ -84,6 +84,7 @@ def lasso_loss(X, y, w, b, alpha):
 
     return y_hat, loss, dw, db
 
+# ridge回归损失函数
 def ridge_loss(X, y, w, b, alpha):
     train_num = len(X.shape[0])
     feature_num = len(X.shape[1])
@@ -94,3 +95,74 @@ def ridge_loss(X, y, w, b, alpha):
     db = np.sum(y_hat - y) / train_num
 
     return y_hat, loss, dw, db
+
+# LDA算法实现
+"""
+线性判别分析，是一种有监督的降维技术
+两组数据投影到同一条直线上，优化策略是类内距离尽可能小，类间距离尽可能大
+LDA实现步骤：
+1、对训练集按照类别进行分组
+2、分别计算每组样本的协方差
+3、计算类间散度矩阵Sw
+4、计算两类样本的均值差μ0-μ1
+5、对类间散度矩阵Sw进行奇异值分解，并求其逆
+6、根据Sw-1(μ0-μ1)得到W
+7、最后计算投影后的数据点Y=WX
+"""
+class LDA:
+    def __init__(self):
+        # 初始化权重矩阵
+        self.w = None
+
+    # 协方差矩阵
+    def calc_cov(self, X, Y=None):
+        m = X.shape[0]
+        # 数据标准化
+        X = (X - np.mean(X, axis=0))/np.std(X, axis=0)
+        if Y == None:
+            Y = X
+        else:
+            (Y-np.mean(Y, axis=0))/np.std(Y, axis=0)
+        return 1/m * np.matmul(X.T, Y)    # matmul计算协方差矩阵
+
+    # 数据投影方法
+    def project(self, X, y):
+        # LDA拟合获取模型权重
+        self.fit(X, y)
+        # 数据投影
+        X_projection = X.dot(self.w)
+        return X_projection
+
+    # LDA拟合方法
+    def fit(self, X, y):
+        # (1)按类分组
+        X0 = X[y == 0]
+        X1 = X[y == 1]
+        # (2)分别计算两个类别数据自变量的协方差矩阵
+        sigma0 = self.calc_cov(X0)
+        sigma1 = self.calc_cov(X1)
+        # (3)计算类内散度矩阵
+        Sw = sigma0 + sigma1 
+        # (4)分别计算两类数据自变量的均值、差
+        u0, u1 = np.mean(X0, axis=0), np.mean(X1, axis=0)
+        mean_diff = np.atleast_1d(u0, u1)
+        # (5)对类内三段矩阵进行奇异值分解
+        U, S, V = np.linalg.svg(Sw)
+        # (6)计算类内散度矩阵的逆
+        Sw_ = np.dot(np.dot(V.T, np.linalg.pinv(np.diag(S))), U.T)
+        # (7)计算w
+        self.w = Sw_.dot(mean_diff)
+        
+    # LDA分类预测
+    def predict(self, X):
+        # 初始化预测结果为空列表
+        y_pred = []
+        # 遍历待预测样本
+        for x_i in X:
+            # 预测模型
+            h = x_i.dot(self.w)
+            y = 1*(h<0)
+            y_pred.append(y)
+        return y_pred
+
+
